60908eae2b10edcd9d0d472453665c57a6258856
alireza <alireza.ra1996@gmail.com>
2020-10-06 21:34:49 +0330

    Update constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,3 @@
 export const BOT_ID = "@soundfy_bot"
 export const STORAGE_CHANNEL_USERNAME = "@soundfystorage";
-export const DEV_DB_URL =
-  "mongodb+srv://user_soundfy:en1EiKaPBbJmzztP@soundfy-fhiih.azure.mongodb.net/test?retryWrites=true&w=majority";
+export const DEV_DB_URL = "";
